e60cedb906fea1d19a850196df4289d0d96aa994
nachos101 <nachostrau17@gmail.com>
2026-09-23 17:35:10 -0300

    minor changes

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,10 @@ Route::get('products/{product}', [ProductController::class, 'show']);
 
 // Rutas protegidas (requieren autenticación) - Crear, actualizar, eliminar
 Route::middleware('auth:sanctum')->group(function () {
-
+    // Rutas de autenticación
     Route::post('logout', [AuthController::class, 'logout']);
     Route::get('user', [AuthController::class, 'user']);
-
+    // Rutas de productos
     Route::post('products', [ProductController::class, 'store']);
     Route::put('products/{product}', [ProductController::class, 'update']);
     Route::delete('products/{product}', [ProductController::class, 'destroy']);
